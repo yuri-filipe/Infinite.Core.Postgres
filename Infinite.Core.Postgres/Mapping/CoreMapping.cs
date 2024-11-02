b3cc7127913da12f1eb3b25edd58097a989cd3bb
Yuri Filipe <yurifelipe64@hotmail.com>
2024-11-02 17:17:28 -0300

    Adicionando mapeamento automáico

--- Infinite.Core.Postgres/Mapping/CoreMapping.cs
+++ Infinite.Core.Postgres/Mapping/CoreMapping.cs
@@ -2,11 +2,24 @@
 {
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+
     public abstract class CoreMapping<TEntity, TKey> : IEntityTypeConfiguration<TEntity>
-     where TEntity : CoreEntity<TKey>
+        where TEntity : CoreEntity
     {
         protected abstract string Schema { get; }
         protected abstract string TableName { get; }
+
+        private readonly string[] fixedProperties =
+        {
+            nameof(CoreEntity.UsuarioAlteracao),
+            nameof(CoreEntity.DataAlteracao),
+            nameof(CoreEntity.UsuarioInclusao),
+            nameof(CoreEntity.DataInclusao),
+            nameof(CoreEntity.Excluido)
+        };
 
         public void Configure(EntityTypeBuilder<TEntity> builder)
         {
@@ -14,36 +27,68 @@
 
             builder.HasKey(e => e.Id);
 
-            builder.Property(e => e.Id)
-                .HasColumnName("ID")
-                .ValueGeneratedOnAdd()
-                .HasColumnOrder(0);
+            // Configuração adicional específica de cada entidade
+            ConfigureAdditionalProperties(builder);
 
-            builder.Property(e => e.Excluido)
-                .HasColumnName("EXCLUIDO")
-                .HasDefaultValue(false)
-                .HasColumnOrder(1);
+            // Configuração dinâmica das propriedades, exceto as fixas
+            var allProperties = typeof(TEntity).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            int order = 1;
+
+            foreach (var property in allProperties)
+            {
+                // Pula as propriedades fixas
+                if (fixedProperties.Contains(property.Name)) continue;
+
+                builder.Property(property.Name)
+                    .HasColumnName(ConvertToColumnFormat(property.Name))
+                    .HasColumnOrder(order++);
+            }
+
+            // Configurando as colunas fixas no final
+            ConfigureFixedProperties(builder, ref order);
+        }
+
+        private void ConfigureFixedProperties(EntityTypeBuilder<TEntity> builder, ref int order)
+        {
+            builder.Property(e => e.UsuarioAlteracao)
+                .HasColumnName("USUARIO_ALTERACAO")
+                .IsRequired(true)
+                .HasDefaultValue("-")
+                .HasMaxLength(100)
+                .HasColumnOrder(order++);
+
+            builder.Property(e => e.DataAlteracao)
+                .HasColumnName("DATA_ALTERACAO")
+                .IsRequired(true)
+                .HasColumnOrder(order++);
 
             builder.Property(e => e.UsuarioInclusao)
                 .HasColumnName("USUARIO_INCLUSAO")
                 .HasMaxLength(100)
-                .HasColumnOrder(2);
+                .IsRequired(true)
+                .HasDefaultValue("-")
+                .HasColumnOrder(order++);
 
             builder.Property(e => e.DataInclusao)
                 .HasColumnName("DATA_INCLUSAO")
-                .HasColumnOrder(3);
+                .IsRequired(true)
+                .HasColumnOrder(order++);
 
-            builder.Property(e => e.UsuarioAlteracao)
-                .HasColumnName("USUARIO_ALTERACAO")
-                .HasMaxLength(100)
-                .HasColumnOrder(4);
-
-            builder.Property(e => e.DataAlteracao)
-                .HasColumnName("DATA_ALTERACAO")
-                .HasColumnOrder(5);
-
-            ConfigureAdditionalProperties(builder);
+            builder.Property(e => e.Excluido)
+                .HasColumnName("EXCLUIDO")
+                .IsRequired(true)
+                .HasDefaultValue(false)
+                .HasColumnOrder(order++);
         }
+
         protected abstract void ConfigureAdditionalProperties(EntityTypeBuilder<TEntity> builder);
+
+        private string ConvertToColumnFormat(string propertyName)
+        {
+            // Usa regex para adicionar o padrão TESTE_TESTE baseado no nome da propriedade
+            var formattedName = Regex.Replace(propertyName, "([a-z])([A-Z])", "$1_$2").ToUpper();
+            return formattedName;
+        }
     }
 }
